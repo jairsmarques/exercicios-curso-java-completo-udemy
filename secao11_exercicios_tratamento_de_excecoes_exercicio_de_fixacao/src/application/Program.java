60c1a2cdcb7e95a2ce78f348888811eab5b23a54
Jair Marques <jairsmarques1126@gmail.com>
2026-02-10 09:37:29 -0300

    resolvendo o exercício de fixação da aula sobre tratamento de exceções(solução ruim - tudo no programa principal)

---
+++ secao11_exercicios_tratamento_de_excecoes_exercicio_de_fixacao/src/application/Program.java
@@ -0,0 +1,52 @@
+/*
+ * Fazer um programa para ler os dados de uma conta bancária e depois realizar um saque nesta conta 
+ * bancária, mostrando o novo saldo. Um saque não pode ocorrer ou se não houver saldo na conta, ou se o 
+ * valor do saque for superior ao limite de saque da conta.
+ * 
+ */
+
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Informe os dados da conta:");
+		System.out.print("Número: ");
+		int number = sc.nextInt();
+		System.out.print("Titular: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Saldo inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+
+		System.out.println();
+		System.out.print("Informe a quantia para sacar: ");
+		double amount = sc.nextDouble();
+
+		if (amount > acc.getWithdrawLimit()) {
+			System.out.println("Erro de saque: A quantia excede o limite de saque");
+		} else if (amount > acc.getBalance()) {
+			System.out.println("Erro de saque: saldo insuficiente");
+		} else {
+			acc.withdraw(amount);
+			System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+		}
+
+		sc.close();
+
+	}
+
+}
